524724a6d377df34d1bd8dd32963f5e39db3f3bb
Evgenia <evgenia.github@gmail.com>
2022-11-01 12:12:52 +0100

    change function binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -1,20 +1,26 @@
+
+
+function binarySearch(array, item){
+    let low = 0; 
+    let high = array.length - 1;
+
+    while(low <= high){
+        let middle = Math.floor((low + high) / 2);
+        let guess = array[middle];
+        if(guess === item){
+             console.log(middle);
+             return middle;
+        }
+        if(guess > item){
+         high = middle - 1
+        }
+        else{
+         low = middle + 1
+        }
+        return null;
+     }
+}
+
 let array = [12,13,34,56,465,2024,3023];
 
-let item = 13;
-let low = 0; 
-let high = array.length - 1;
-
-while(low <= high){
-   let middle = Math.floor((low + high) / 2);
-   let guess = array[middle];
-   if(guess === item){
-        console.log(middle);
-        return middle;
-   }
-   if(guess > item){
-    high = middle - 1
-   }
-   else{
-    low = middle + 1
-   }
-}
+console.log(binarySearch(array, 13))
